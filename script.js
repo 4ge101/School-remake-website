98c603d77338dd4af5557c40968047dfff8aab23
4ge101 <theforgettenone2@gmail.com>
2024-09-11 11:31:24 +0545

    making new down menu

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 // making nav active class
 let nav = document.querySelector('nav');
-let li_items = document.querySelectorAll('li')
-li_items.forEach(item => {
+let items = document.querySelectorAll('li');
+items.forEach(item => {
     item.onclick = () => {
         let oldActive = nav.querySelector('li.active');
-        if (oldActive) oldActive.classList.remove('active');
+        if (oldActive) oldActive.classList.remove('active')
         item.classList.add('active');
+    nav.style.setProperty('--position-x-active', item.offsetLeft + 'px');
     }
 })
+
+let itemAcitve = nav.querySelector('li.active')
+if (itemAcitve) nav.style.setProperty('--position-x-active', itemAcitve.offsetLeft + 'px')
 
 
 // making slider which change background 
