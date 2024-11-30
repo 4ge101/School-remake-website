741628a36b5aa473653107f80bd39af3e89ef65d
4ge101 <theforgettenone2@gmail.com>
2024-11-30 18:52:46 +0545

    making footer

--- script.js
+++ script.js
@@ -77,3 +77,13 @@ function moveSlider(direction) {
         }
     }, { once: true })
 }
+
+// making goup after clicking icon
+
+let pageUpButton = document.getElementById("goUp");
+pageUpButton === null || pageUpButton === void 0 ? void 0 : pageUpButton.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
