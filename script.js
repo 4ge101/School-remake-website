cd32efc2d621f088df53076d5dd75f2241366ead
4ge101 <theforgettenone2@gmail.com>
2024-12-01 19:40:05 +0545

    removing student counter

--- script.js
+++ script.js
@@ -87,20 +87,3 @@ pageUpButton === null || pageUpButton === void 0 ? void 0 : pageUpButton.addEven
         behavior: "smooth"
     });
 });
-
-// making student and teacher counter
-
-let valueDisplays = document.querySelectorAll(".counter-number");
-let interval = 4000;
-valueDisplays.forEach((valueDisplay) => {
-    let startValue = 0;
-    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
-    let duration = Math.floor(interval / endValue);
-    let counter = setInterval(function () {
-        startValue += 1;
-        valueDisplay.textContent = startValue;
-        if (startValue == endValue) {
-            clearInterval(counter);
-        }
-    }, duration);
-});
